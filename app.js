194d9274cddf6909821ac0868aef0afc547d010d
contrapuntist <macano661@gmail.com>
2018-02-06 22:15:54 -0600

    initial commit with basic file

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log('starting app');
+
+// file system and OS module 
+// fs.appendFile 
+
+const fs = require('fs');
+const os = require('os');
+const notes = ('/notes.js');
+
+
+
+var user = os.userInfo();
+
+
+fs.appendFileSync('greetings.txt', `Hello ${user.username}!`); 
+// fs.appendFile("greetings.txt", "Hello World", function(err) { 
+//     if (err) { 
+//         console.log("Err message: " + err);
+//     }
+// })
