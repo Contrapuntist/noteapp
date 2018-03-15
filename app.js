944f932087c8903537afd3a705b9eaa1a69e189f
contrapuntist <macano661@gmail.com>
2018-03-14 22:41:48 -0500

    edits to app and notes files

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-console.log('starting app');
+console.log('starting app'); 
 
 // file system and OS module 
 // fs.appendFile 
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2]; 
-console.log('Command: '+ command); 
-console.log(process.argv);
+const argv = yargs.argv;
+var command = argv._[0]; 
+console.log("Command: " + command); 
+// console.log("process: " + process.argv);
+console.log("Yargs: ",  argv);
 
 if (command === 'add') { 
-    console.log('adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
     console.log('listing all notes');
+    notes.getAll();
 } else if (command === 'read') {
     console.log('reading these notes');
+    notes.getNote(argv.title);
 } else if (command === 'remove') {
     console.log('Note has been removed');
+    notes.removeNote(argv.title);
 } else { 
     console.log('command not recognized');
 } 
--- notes.js
+++ notes.js
@@ -1,6 +1,24 @@
 console.log('starting notes.js');
 
-module.exports.addNote = () => { 
-	console.log('addNote');
-	return 'new Note';
-};
+var addNote = (title, body) => { 
+	console.log("adding note", title, body);
+}; 
+
+var getAll = () => {
+	console.log('getting all notes');
+}
+
+var getNote = (title) => { 
+	console.log("getting this note", title );  
+}
+
+var removeNote = (title) => { 
+	console.log("removing this note", title);
+}
+
+module.exports = { 
+	addNote, 
+	getAll,
+	getNote,
+	removeNote
+}
